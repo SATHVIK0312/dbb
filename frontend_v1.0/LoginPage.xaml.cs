0096238a87d3be7c7591589f7c0e7795a711ef19
SATHVIK SANGARAJU <98736404+SATHVIK0312@users.noreply.github.com>
2025-11-21 22:29:29 +0530

    Add files via upload

---
+++ frontend_v1.0/LoginPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using jpmc_genai.Services;
+
+namespace jpmc_genai
+{
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using var client = new ApiClient();
+                var login = new LoginCreate { username = UsernameTextBox.Text, password = PasswordBox.Password };
+                var content = new StringContent(JsonSerializer.Serialize(login), Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("Login", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    var resp = JsonSerializer.Deserialize<LoginResponse>(await response.Content.ReadAsStringAsync());
+                    Session.CurrentUser = resp;
+                    Session.Token = resp.token;
+                    client.SetBearer(resp.token);
+                    this.NavigationService.Navigate(new ProjectPage());
+                }
+                else
+                {
+                    MessageBox.Show("Login failed: " + response.ReasonPhrase);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new RegisterPage());
+        }
+    }
+}
---
+++ frontend_v1.0/LoginWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace jpmc_genai
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new LoginPage());
+        }
+    }
+}
