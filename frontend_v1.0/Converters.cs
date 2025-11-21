b06697a9f99afd52d3dce45020c3aedbc9f8928a
SATHVIK SANGARAJU <98736404+SATHVIK0312@users.noreply.github.com>
2025-11-21 23:30:45 +0530

    Create Converters.cs

---
+++ frontend_v1.0/Converters.cs
@@ -0,0 +1,42 @@
+// Converters.cs
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace JPMCGenAI_v1._0
+{
+    public class InvertedBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string text)
+            {
+                return string.IsNullOrEmpty(text) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class InverseStringNullOrEmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string text)
+            {
+                return string.IsNullOrEmpty(text) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
