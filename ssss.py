b607aebad06f273b0c54f8289a1ce4b48104d843
SATHVIK SANGARAJU <98736404+SATHVIK0312@users.noreply.github.com>
2025-12-11 05:52:04 +0530

    Update ssss.py

--- ssss.py
+++ ssss.py
@@ -2,34 +2,34 @@
 Feature: SauceDemo - Complete Purchase Flow as standard_user
 
   Scenario: TC_SAUCE_001 - End-to-end purchase with 3 items and logout
-    Given I navigate to SauceDemo login page "https://www.saucedemo.com"
-    When I enter username "standard_user"
-    And I enter password "secret_sauce"
-    And I click the Login button
-    Then I am redirected to the inventory page with URL containing "/inventory.html"
+    Given User navigates to SauceDemo login page "https://www.saucedemo.com"
+    When User enters username "standard_user"
+    And User enters password "secret_sauce"
+    And User clicks the Login button
+    Then User is redirected to the inventory page with URL containing "/inventory.html"
     And page title contains "Swag Labs"
-    When I select sorting option "Price (high to low)"
+    When User selects sorting option "Price (high to low)"
     Then the first product price is "$49.99"
-    When I click Add to cart on the first item
-    And I click Add to cart on the second item
-    And I click Add to cart on the third item
+    When User clicks Add to cart on the first item
+    And User clicks Add to cart on the second item
+    And User clicks Add to cart on the third item
     Then the shopping cart badge displays "3"
-    When I click the shopping cart icon
+    When User clicks the shopping cart icon
     Then the cart page URL contains "cart.html"
     And exactly 3 items are listed in the cart
-    When I click Checkout button
-    Then I am on the checkout step one page
-    When I enter First Name "Automation"
-    And I enter Last Name "Tester"
-    And I enter Zip/Postal Code "11001"
-    And I click Continue button
-    Then I am on checkout overview page
+    When User clicks Checkout button
+    Then User is on the checkout step one page
+    When User enters First Name "Automation"
+    And User enters Last Name "Tester"
+    And User enters Zip/Postal Code "11001"
+    And User clicks Continue button
+    Then User is on checkout overview page
     And Payment Information section shows "SauceCard #31337"
     And Shipping Information shows "Free Pony Express Delivery!"
     And Item total is greater than "$50"
-    When I click Finish button
+    When User clicks Finish button
     Then checkout complete page shows header "Thank you for your order!"
     And success message contains "Your order has been dispatched"
-    When I open the burger menu
-    And I click Logout
-    Then I am redirected back to login page
+    When User opens the burger menu
+    And User clicks Logout
+    Then User is redirected back to login page
